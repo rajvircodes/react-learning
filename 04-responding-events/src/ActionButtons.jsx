a5bd2503b371f2a062db65b2143e55805e6ce33a
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-03-23 23:26:33 +0530

    events

---
+++ 04-responding-events/src/ActionButtons.jsx
@@ -0,0 +1,13 @@
+function ActionButtons(){
+  const handlehello = ()=>{
+    alert("Hello");
+  }
+
+
+
+  return <>
+    <button onClick={handlehello}>Say Hello</button>
+  </>
+}
+
+export default ActionButtons
---
+++ 04-responding-events/src/App.jsx
@@ -0,0 +1,8 @@
+import ActionButtons from "./ActionButtons";
+function App(){
+  return <>
+    <ActionButtons />
+    </>
+}
+
+export default App;
