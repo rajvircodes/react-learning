f9c4bda579f102b4c707b6a1d44cc8529511c388
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-04-22 08:37:13 +0530

    expense-trackerz

---
+++ 07-expense-tracker/src/components/AddTransaction.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useState } from 'react'
+const AddTransaction = () => {
+
+  const [text, setText] = useState('')
+  const [amount, setAmount] = useState(0)
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+
+      <form >
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input type="text"
+            value={text} onChange={(e) => setText(e.target.value)}
+            id="text" placeholder="Enter text..." />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">Amount <br />
+            (negative - expense, positive - income)</label>
+          <input
+
+            type="number"
+            value={amount} onChange={(e) => setAmount(e.target.value)}
+            id="amount" placeholder="Enter amount..." />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+
+    </>
+  )
+}
+
+export default AddTransaction
